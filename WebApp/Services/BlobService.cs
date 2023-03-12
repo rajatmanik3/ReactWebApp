e778748a069f3bd7d8bceb89e987ec4d71102e57
Rajat Manik <rajatmanik3@gmai.com>
2023-03-12 16:20:34 +0530

    Blob service for storing images in azure storage

--- WebApp/Services/BlobService.cs
+++ WebApp/Services/BlobService.cs
@@ -1,28 +1,47 @@
 ﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using WebApp.Services;
 
-namespace WebApp.Services
+namespace RedMango_API.Services
 {
     public class BlobService : IBlobService
     {
         private readonly BlobServiceClient _blobClient;
 
-        public BlobService(BlobServiceClient blobClient) 
+        public BlobService(BlobServiceClient blobClient)
         {
             _blobClient = blobClient;
         }
-        public Task<string> DeleteBlob(string blobName, string containerName)
+        public async Task<bool> DeleteBlob(string blobName, string containerName)
         {
-            throw new NotImplementedException();
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            return await blobClient.DeleteIfExistsAsync();
+
         }
 
-        public Task<string> GetBlob(string blobName, string containerName)
+        public async Task<string> GetBlob(string blobName, string containerName)
         {
-            throw new NotImplementedException();
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            return blobClient.Uri.AbsoluteUri;
         }
 
-        public Task<string> UpdateBlob(string blobName, string containerName, IFormFile file)
+        public async Task<string> UploadBlob(string blobName, string containerName, IFormFile file)
         {
-            throw new NotImplementedException();
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            var httpHeaders = new BlobHttpHeaders()
+            {
+                ContentType = file.ContentType
+            };
+            var result = await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+            if (result != null)
+            {
+                return await GetBlob(blobName, containerName);
+            }
+            return "";
         }
     }
 }
